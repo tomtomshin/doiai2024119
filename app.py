b939d6b4b3f3dfe6a2fa47ce97c1a724ac1c65d9
tomtomshin <stom1962@gmail.com>
2024-11-09 09:51:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 # 以下を「app.py」に書き込み
 import streamlit as st
 import openai
-import secret_keys  # 外部ファイルにAPI keyを保存
 
-openai.api_key = secret_keys.openai_api_key
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 system_prompt = """
 このスレッドでは以下ルールを厳格に守ってください。
